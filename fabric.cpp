30a0b2ff6fac146783bc428a46d11b026c9f5607
Aditya Ultra <adityaultra4@gmail.com>
2016-05-14 16:43:11 +0530

    refractored a lot of code, Now the library is fully functional :smile:

--- fabric.cpp
+++ fabric.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 
 using namespace std;
 
@@ -8,90 +9,111 @@ class Todo{
 public:
     char content[200];
     char status;
-    int _index;
+     int _index;
 };
 
 class List {
   public:
     Todo list[20];
-    int fill;
+     int _listIndex;
+     int _tagIndex;
     char title[100];
     char tags[10][20];
 
-    list() {
-        fill = 0;
+    List() {
+        _listIndex = 0;
+         _tagIndex = 0;
+
+        cout << "Enter title for your list\n *> ";
+        cin.getline(title, sizeof(title));
+
+        char choice;
+        cout << "Do you want some tags?(y/n)" << endl << " *> ";
+        cin >> choice;
+        cin.ignore();
+
+        if (choice == 'y') {
+            char tagTemp[220];
+            int k = 0, j = 0;
+
+            cout << "Enter the tag one at a time \n 'd' when done \n *> ";
+
+            while (1) {
+                cin.getline(tagTemp, 20);
+
+                if (strcmp(tagTemp, "d") != 0) {
+                    addTag(tagTemp);
+                    cout << " *> ";
+                }
+
+                else {
+                    break;
+                }
+
+
+            }
+        }
     }
 
-    list(char title[100]) {
-        fill = 0;
-        strcpy(this->title, title);
+    List(char Title[100]) {
+        _listIndex = 0;
+         _tagIndex = 0;
+
+        strcpy(title, Title);
     }
 
-    void setTags(char tags[10][20]) {
-        memcpy(this->tags, tags, sizeof(tags));
+    void addTag(char Tag[20]) {
+        strcpy(tags[_tagIndex++], Tag);
     }
 
-    void setTitle(char title[100]) {
-        strcpy(this->title, title);
+    void setTitle(char Title[100]) {
+        strcpy(title, Title);
     }
 
     void view() {
-        for (size_t i = 0; i < fill; i++) {
-            cout << "[" << list[i].status << "]\t";
+        cout << title << endl;
+        cout << "============" << endl;
+
+        cout << "Tags : ";
+        for (size_t i = 0; i < _tagIndex; i++) {
+            cout << tags[i] << " ";
+        }
+
+        cout << endl;
+
+        for (int i = 0; i < _listIndex; i++) {
+            cout << "  [" << list[i].status << "]  ";
             cout << list[i].content << endl;
         }
     }
 
     void append() {
-        char content[200];
+        char Content[200];
         cout << "Enter the content of ToDo \n *> ";
-        cin.getline(content, 200);
+        cin.getline(Content, 200);
+        cout << "content is : " << Content << endl;
 
-        strcpy(list[fill].content, content);
-        list[fill].status = ' ';
-        list[fill]._index = fill;
+        strcpy(list[_listIndex].content, Content);
+        list[_listIndex].status = ' ';
+        list[_listIndex]._index = _listIndex;
+        _listIndex++;
     }
 
     void changeStatus(int index, char status){
         list[index].status = status;
     }
 
-    ~list() {}
+    ~List() {}
 };
-
-void newList(List &myList){
-    char title[100];
-
-    cout << "Enter title for your list\n *> ";
-    myList.setTitle(title);
-
-    char choice;
-    cout << "Do you want some tags?(y/n)" << endl << " *> ";
-
-    if (choice == 'y') {
-        char tags[10][20];
-        char tagLine[220];
-        int k = j = 0;
-
-        cout << "Enter the tags, seperated by spaces" << endl << " *> ";
-        cin.getline(tagLine, 220);
-
-        for (size_t i = 0; i < 220; i++) {
-            if (tagLine[i] != ' ') {
-                tags[j][k++] = tagLine[i];
-            }
-            else {
-                j++;
-                k = 0;
-            }
-        }
-
-        myList.setTags(tags);
-    }
-}
 
 int main() {
     // NOTE
     //      This main function is just for testing the stuff
+    List Myl;
+
+    Myl.append();
+    Myl.append();
+
+    Myl.view();
     return 0;
 }
