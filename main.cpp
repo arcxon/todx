8373731b04e4ba01c1bf66f0feb62b9e10e850dd
Aditya Ultra <adityaultra4@gmail.com>
2016-05-19 23:39:25 +0530

    Added better comments, save and quit fxns, fxn declarations :smile:

--- main.cpp
+++ main.cpp
@@ -4,11 +4,19 @@
 using namespace std;
 
 List arrayL[20];
-int _arrayLindex = 0;
 List currentL;
+int _arrayLindex = 0;
+int _currentLindex;
 int isOpenL = 0;
 
+// Fxns Used
+
+void initiate();
+int parse(char []);
+void finish();
+
 void initiate() {
+    // Fxn to read data from external file int the array of Lists
     fstream file("data.tdx", ios::in|ios::out|ios::binary);
     List tempOb; // temperary object
 
@@ -20,6 +28,7 @@ void initiate() {
 }
 
 int parse(char command[80]){
+    // Fxn to Parse(execute) the command passed
     /// Here a crazy fun expression is used
     /// Since we wanted any of them to be zero they were multiplied
     /// to give zero. Mathematically it is quite accurate and syntactically
@@ -28,26 +37,36 @@ int parse(char command[80]){
     int success = 0;
 
     if (!(strcmp(command, "new")*strcmp(command, "n"))) {
+        // Create a new List
         arrayL[_arrayLindex++].enter();
         success = 1;
     }
 
     else if (!(strcmp(command, "open")*strcmp(command, "o"))) {
+        // Open existing lists
         int choice;
         cout << "Enter The No. of List to be opened" << endl;
+
         for (int i = 0; i < _arrayLindex; i++) {
             cout << i << " " << arrayL[i].title << endl;
         }
+
         cin >> choice;
         cin.ignore();
 
-        currentL = arrayL[choice];
+        if ( 0 <= choice && choice <= _arrayLindex) {
+            currentL = arrayL[choice];
+            _currentLindex = choice;
+
+            cout << "Opened : " << currentL.title << endl;
+            currentL.view();}
 
         isOpenL = 1;
         success = 1;
     }
 
     else if (!(strcmp(command, "append")*strcmp(command, "a"))) {
+        // Append a new Todo to the currently opened list
         if (isOpenL) {
             currentL.append();
         }
@@ -58,6 +77,7 @@ int parse(char command[80]){
     }
 
     else if (!(strcmp(command, "view")*strcmp(command, "v"))) {
+        // view todo's of the current list
         if (isOpenL) {
             currentL.view();
         }
@@ -67,11 +87,24 @@ int parse(char command[80]){
         success = 1;
     }
 
+    else if (!(strcmp(command, "save")*strcmp(command, "s"))) {
+        // Append a new Todo to the currently opened list
+        arrayL[_currentLindex] = currentL;
+        finish();
+        success = 1;
+    }
+
+    else if (!(strcmp(command, "quit")*strcmp(command, "q"))) {
+        arrayL[_currentLindex] = currentL;
+        finish();
+        success = -1;
+    }
 
     return success;
 }
 
 void finish() {
+    // Write the changes back to file
     ofstream file ("data.tdx", ios::out);
     for (int i = 0; i < _arrayLindex; i++) {
         file.write((char*)&arrayL[i], sizeof(arrayL[i]));
@@ -86,9 +119,16 @@ int main() {
     for (size_t i = 0; i < 4; i++) {
         cout << " *> ";
         cin.getline(command, 80);
-        parse(command);
+        int result = parse(command);
+        if(result > 0)
+            continue;
+        else if(!result) {
+            cout << "Internal Error" << endl;
+        }
+        else if (result == -1) {
+            break;
+        }
     }
 
-    finish();
     return 0;
 }
